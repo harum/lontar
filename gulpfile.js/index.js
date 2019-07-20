d2279d01c7cb90768ead3a0e9a47bba5d369bbd1
Putu Harum Bawa <lightharum.108@gmail.com>
2019-07-20 16:19:19 +0700

    tidy up design system task

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,27 +1,44 @@
+const gulp = require('gulp');
 const pagesGenerator = require('./pagesGenerator');
 const cssGenerator = require('./cssGenerator');
+const jsGenerator = require('./jsGenerator');
 
 function designSystem() {
   const pageSetting = {
-    dest: './projects/designSystem/www',
-    src: ['./projects/designSystem/docs/*.{html,hbs}'],
-    partials: ['./projects/designSystem/docs/partials/**/*.{html,hbs}'],
+    dest: 'projects/designSystem/www',
+    src: ['projects/designSystem/docs/*.{html,hbs}'],
+    partials: ['projects/designSystem/docs/partials/**/*.{html,hbs}'],
   };
 
   const cssSetting = {
-    dest: './projects/designSystem/www/css',
-    src: ['./projects/designSystem/scss/*.scss'],
-    watch: ['./projects/designSystem/scss/**/*.scss'],
+    dest: 'projects/designSystem/www/css',
+    src: ['projects/designSystem/scss/*.scss'],
+    watch: ['projects/designSystem/scss/**/*.scss'],
   };
 
-  const pg = pagesGenerator(pageSetting);
-  const cg = cssGenerator(cssSetting);
+  const jsSetting = {
+    dest: 'projects/designSystem/www/js',
+    src: ['projects/designSystem/js/**/*.js'],
+    entry: 'projects/designSystem/js/index.js',
+  };
 
-  pg.generate();
-  pg.watch();
-  cg.generate();
-  cg.watch();
+  const page = pagesGenerator(pageSetting);
+  const css = cssGenerator(cssSetting);
+  const js = jsGenerator(jsSetting);
 
+  return gulp.series(
+    gulp.parallel(
+      page.generatePage,
+      css.generateCss,
+      js.generateJsModule,
+      js.generateJsBundle,
+    ),
+    gulp.parallel(
+      page.watch,
+      css.watch,
+      js.watch,
+    ),
+  );
 };
 
-exports.designSystem = designSystem;
+exports.designSystem = designSystem();
